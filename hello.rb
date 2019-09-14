5df4c492b96e89f349344f4a67aed6645153161f
satoshi <inu800ss@gmail.com>
2019-09-14 01:31:42 +0000

    first

---
+++ hello.rb
@@ -0,0 +1,99 @@
+puts "hello ruby !!!!!!"
+puts "Branding Engineer"
+puts "Hello"
+puts "HR Tech"
+
+p 1
+p 2
+p 3
+
+puts "hello ruby" + "hello" + "teck boost"
+
+puts 2+2
+puts 2-2
+puts 2*2
+puts 2/2
+puts "1"+"1"
+
+date = 20190913
+p date
+
+fruits = ["リンゴ","メロン","バナナ","イチゴ"]
+
+p fruits
+
+array =[1,2,3,4]
+p array[0]
+p array[1]
+p array[2]
+p array[3]
+
+
+fruits={name:"リンゴ",price:100}
+p fruits[:name]
+p fruits[:price]
+
+p "リンゴ".length
+p "ruby".upcase
+p "12".to_i
+p "ruby"[2]
+p "hello,ruby,world".split(',')
+
+p 1+1
+p 12.to_s
+p 2.next
+p 1.pred
+p 1.zero?
+p 0.nonzero?
+
+def print_fruits(name)
+  p name
+end 
+
+ print_fruits("リンゴ")
+ print_fruits("バナナ")
+ 
+ puts "初めてのRUBY"
+ puts "RUBY"+"始めました"
+ puts 3+3
+ puts 3-3
+ puts 3*3
+ puts 3/3
+ 
+p "1,2,3,4".split(',')
+ 
+ nam_array =["1","2","3","4"]
+ p nam_array
+ 
+ sat ={name:'satoshi',birthday:'19620613',blood:'B'}
+ p sat[:name]
+ p sat[:birthday]
+ p sat[:blood]
+ 
+def plus_ruby(name)
+ p name+"ruby"
+end 
+
+plus_ruby("like")
+plus_ruby("yhee")
+
+def plus_one(num)
+  p num+1
+end 
+
+plus_one(4)
+plus_one(10)
+
+ 
+
+ 
+ 
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
