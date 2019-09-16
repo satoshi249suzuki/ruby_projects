47491215bfeea187e3c9a9faf8c7d94a4decd7a3
satoshi <inu800ss@gmail.com>
2019-09-16 06:43:43 +0000

    2nd

---
+++ Ruby03.rb
@@ -0,0 +1,37 @@
+puts "hello sat"
+
+n = 3
+if n == 3
+  p 'nは３です'
+end 
+
+n =4
+if n ==3 || n == 4
+  p 'nは３か４どちらかです'
+end 
+
+n =3
+if n == 3
+  p 'nは３です'
+elsif n == 4
+ p 'nは４です'
+end 
+
+for k in 1..5 do
+  p k = "こんにちは"
+end
+
+[1,2,3,4,5].each do |num|
+  p num
+end 
+
+n = 4
+if n ==2
+  p 'nは２です'
+elsif n == 3
+  p 'nは３です'
+else
+  p 'それ以外です'
+end 
+
+
